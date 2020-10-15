e77d2527fc21d499bb67ff2e94aba893a0ebff4e
Bhaskar Acharya <bhaskar_acharya1999@rediffmail.com>
2020-10-15 20:29:44 +0530

    first commit

---
+++ src/app/todo-details/todo-details.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {Todo} from '../todo';
+
+@Component({
+  selector: 'app-todo-details',
+  templateUrl: './todo-details.component.html',
+  styleUrls: ['./todo-details.component.css']
+})
+export class TodoDetailsComponent implements OnInit {
+
+  @Input() todo:Todo;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
